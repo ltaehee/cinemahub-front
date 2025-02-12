76ee0054e8ab50be181f7f59a0fef4d435297884
seyoon <se981106@naver.com>
2025-02-12 21:52:27 +0900

    Feat: header 작성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,26 @@
+import HeaderIcon from "../icons/HeaderIcon";
+
 const Header = () => {
-  return <header>헤더</header>;
+  return (
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center border-b border-slate-300">
+        <div className="flex justify-between items-center gap-8 px-8 py-2 h-16 w-1280">
+          <div className="flex items-center gap-8">
+            <button>
+              <HeaderIcon className="w-full" />
+            </button>
+            <button>박스오피스</button>
+          </div>
+
+          <div>
+            <div>
+              <button className=""></button>
+            </div>
+            <div>로그인</div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 export default Header;
