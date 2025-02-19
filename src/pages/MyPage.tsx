a195e9696e7bc7da15647a023e7a87c594feff6b
이태희 <taehee@itaehuiui-MacBookAir.local>
2025-02-19 10:26:57 +0900

    feat: 마이 페이지 분기 처리 완료 프론트

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -6,6 +6,18 @@ import profileEdit from "/images/profileEdit.png";
 import profileEdit2 from "/images/profileEdit2.png";
 import { useEffect, useState } from "react";
 import { baseInstance } from "../apis/axios.config";
+import FollowSection from "../components/mypage/FollowSection";
+
+const followerList = [
+  { nickname: "닉네임1", email: "test1@naver.com", profileImg: "" },
+  { nickname: "닉네임2", email: "test2@naver.com", profileImg: "" },
+  { nickname: "닉네임3", email: "test3@naver.com", profileImg: "" },
+];
+
+const followingList = [
+  { nickname: "팔로잉1", email: "follow1@naver.com", profileImg: "" },
+  { nickname: "팔로잉2", email: "follow2@naver.com", profileImg: "" },
+];
 
 interface UserProfile {
   userId: string;
@@ -17,16 +29,13 @@ interface UserProfile {
 
 const MyPage = () => {
   const { nickname } = useParams();
-  const navigate = useNavigate();
   const [isEditing, setIsEditing] = useState(false);
   const [profile, setProfile] = useState<UserProfile | null>(null);
   const [originalProfile, setOriginalProfile] = useState<UserProfile | null>(
     null
   );
   const [isOwnProfile, setIsOwnProfile] = useState(false);
-
-  const loggedInUserName = profile?.nickname;
-  console.log("loggedInUserName", loggedInUserName);
+  const navigate = useNavigate();
 
   /* 프로필 조회 */
   const getProfileData = async () => {
@@ -34,10 +43,10 @@ const MyPage = () => {
       const response = await baseInstance.get(`/mypage/profile/${nickname}`);
 
       if (response.status === 200) {
-        setProfile(response.data); // profile 객체 한 번에 저장
+        setProfile(response.data); // 프로필 데이터 저장
         setOriginalProfile(response.data);
-        setIsOwnProfile(loggedInUserName === nickname);
-        console.log("profile", profile);
+        setIsOwnProfile(response.data.isOwnProfile); // 백엔드에서 받아온 값 사용
+        console.log("Rwds", response.data);
       }
     } catch (err) {
       console.error("프로필 조회 실패:", err);
@@ -46,7 +55,7 @@ const MyPage = () => {
 
   /* 프로필 수정 */
   const updateProfileData = async () => {
-    if (!profile || !originalProfile) return;
+    if (!profile || !originalProfile || !isOwnProfile) return;
     if (
       profile.nickname === originalProfile.nickname &&
       profile.introduce === originalProfile.introduce
@@ -54,10 +63,6 @@ const MyPage = () => {
       console.log("변경된 내용 없음, API 요청 생략");
       return;
     }
-    console.log("1", profile.nickname);
-    console.log("2", originalProfile.nickname);
-    console.log("3", profile.introduce);
-    console.log("4", originalProfile.nickname);
 
     try {
       const response = await baseInstance.patch("/mypage/profile-update", {
@@ -74,14 +79,16 @@ const MyPage = () => {
     }
   };
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (profile && !isEditing) {
-      setIsOwnProfile(true);
+      setIsOwnProfile(loggedInUserName === nickname);
     }
-  }, [profile, isEditing]);
+  }, [profile, loggedInUserName, nickname, isEditing]); */
 
   useEffect(() => {
     getProfileData();
+    console.log("profile", profile);
+    console.log("isOwnProfile", isOwnProfile);
   }, []);
 
   const handleEditClick = () => {
@@ -98,8 +105,8 @@ const MyPage = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <div className="w-full max-w-[1280px] flex gap-2 mt-10 mb-20">
-        <div className="w-full border border-[#DFDFDF] rounded-2xl">
-          <div className="w-full relative flex flex-col gap-2 items-center p-2">
+        <div className="w-full  border border-[#DFDFDF] rounded-2xl">
+          <div className="w-full relative flex flex-col gap-2 items-center p-2 ">
             <div className="w-full flex justify-end mb-4">
               {isOwnProfile ? (
                 <img
@@ -147,26 +154,39 @@ const MyPage = () => {
                 {profile.introduce || "자기소개를 해주세요"}
               </p>
             )}
-            {!isOwnProfile && <Button className=" mt-6">팔로우</Button>}
+            {!isOwnProfile && (
+              <div className="w-full px-12">
+                <Button className=" mt-6">팔로우</Button>
+              </div>
+            )}
           </div>
         </div>
         <div className="w-full flex flex-col gap-2">
-          <div className="h-full flex flex-col justify-center items-center border border-[#DFDFDF] rounded-2xl p-4">
+          <div className="h-full flex flex-col justify-center items-center border border-[#DFDFDF] rounded-2xl p-4 px-12">
             <p className="">팔로워</p>
             <p>110명</p>
-            <Button className="!w-[80%] bg-gray-500 hover:bg-gray-600 disabled:bg-gray-400">
+            <Button className="bg-gray-500 hover:bg-gray-600 disabled:bg-gray-400">
               팔로워 보기
             </Button>
           </div>
-          <div className="h-full flex flex-col justify-center items-center border border-[#DFDFDF] rounded-2xl p-4">
+          <div className="h-full flex flex-col justify-center items-center border border-[#DFDFDF] rounded-2xl p-4 px-12">
             <p className="">팔로잉</p>
             <p>80명</p>
-            <Button className="!w-[80%] bg-gray-500 hover:bg-gray-600 disabled:bg-gray-400">
+            <Button className="bg-gray-500 hover:bg-gray-600 disabled:bg-gray-400">
               팔로잉 보기
             </Button>
           </div>
         </div>
+        {/* <div className="w-full flex flex-col gap-2">
+          <FollowSection
+            followerCount={followerList.length}
+            followingCount={followingList.length}
+            followerList={followerList}
+            followingList={followingList}
+          />
+        </div> */}
       </div>
+
       <TabContainer />
     </div>
   );
