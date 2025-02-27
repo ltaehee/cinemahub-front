ca0c80fb29e1e47aa9b999691567ffe2459295ea
seyoon <se981106@naver.com>
2025-02-27 15:42:09 +0900

    관리자 신고리뷰 작성중

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -4,7 +4,7 @@ import Table from "../components/adminpage/Table";
 import Button from "../components/Button";
 import SearchBar from "../components/adminpage/SearchBar";
 import { getFetchUserInfo } from "../apis/search";
-import { deleteFetchUser, getFetchUser } from "../apis/admin";
+import { getFetchUser, getReportedReview, updateUser } from "../apis/admin";
 import Pagination from "@ui/Pagination";
 
 interface UserProps {
@@ -13,16 +13,25 @@ interface UserProps {
   createdAt: string;
 }
 
+interface ReportProps {
+  email: string; // 신고한 유저의 Id
+  reason: string; // 신고 사유
+  reviewUserId: string; // 신고당한 리뷰 id
+  content: string; // 신고된 리뷰 글
+  imgUrls: string[]; // 신고된 리뷰 이미지
+}
+
 const limit = 4; // 한 페이지에 표시할 항목 수
+const blockSize = 10;
 
 const AdminPage = () => {
   const [users, setUsers] = useState<UserProps[]>([]);
   console.log("users: ", users);
+  const [reportedUser, setReportedUser] = useState<ReportProps[]>([]); // 신고 리뷰 유저
   const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
 
   const [searchQuery, setSearchQuery] = useState<string>(""); // 검색어 상태
   const [isSearching, setIsSearching] = useState(false); // 검색 중인지 여부
-
   const handleSelectUser = (email: string) => {
     setSelectedUsers((prev) =>
       prev.includes(email)
@@ -38,9 +47,10 @@ const AdminPage = () => {
   // 유저 검색
   const handleSearch = async (query: string) => {
     if (query.trim() === "") return;
+    setUsers([]);
     setSearchQuery(query);
     setIsSearching(query.trim() !== ""); // 검색어가 있으면 true, 없으면 false
-    setPage(0);
+    setUserPage(0);
   };
 
   // 유저 삭제
@@ -54,7 +64,7 @@ const AdminPage = () => {
     try {
       console.log("selectedUsers: ", selectedUsers);
 
-      const response = await deleteFetchUser(selectedUsers);
+      const response = await updateUser(selectedUsers);
 
       setUsers((prevUsers) =>
         prevUsers.filter((user) => !selectedUsers.includes(user.email))
@@ -65,39 +75,63 @@ const AdminPage = () => {
     }
   };
 
-  const [_, setActiveIndex] = useState(1);
+  const [activeindex, setActiveIndex] = useState(1);
   const handleChangeTab = (index: number) => {
     setActiveIndex(index);
   };
 
-  const [totalPages, setTotalPages] = useState(0); // 전체 페이지 수
-  const [totalUsers, setTotalUsers] = useState(0); // 총 데이터 수
-  const [page, setPage] = useState(0);
-  const handleChangePage = (page: number) => {
-    setPage(page);
+  // const [totalPages, setTotalPages] = useState(0); // 전체 페이지 수  수정
+  const [totalUsers, setTotalUsers] = useState(0); // 총 유저 데이터 수
+  const [totalReported, setTotalReported] = useState(0); // 신고 리뷰 총 개수
+  const [userPage, setUserPage] = useState(0);
+  const [reviewPage, setReviewPage] = useState(0);
+  // console.log("page: ", page);
+  const handleChangeUserPage = (page: number) => {
+    setUserPage(page);
   };
-
+  const handleChangeReviewPage = (page: number) => {
+    setReviewPage(page);
+  };
   // 유저데이터 가져오기 (검색 or 전체 조회)
   const getUserData = async () => {
     try {
       let response;
       if (isSearching) {
-        response = await getFetchUserInfo(searchQuery, page, limit);
+        response = await getFetchUserInfo(searchQuery, userPage, limit);
+        if (!response) return;
       } else {
-        response = await getFetchUser(page, limit);
+        response = await getFetchUser(userPage, limit);
       }
       console.log("response: ", response);
-      setTotalPages(response.data.totalPages);
+      // setTotalPages(response.data.totalPages); // 수정
       setTotalUsers(response.data.totalCount);
       setUsers(response.data.users);
     } catch (err) {
       console.error(err);
     }
   };
+
+  // 신고 리뷰 전체조회
+  const getReportedReviewsData = async () => {
+    try {
+      const response = await getReportedReview(reviewPage, limit);
+      console.log("신고리뷰 조회: ", response);
+      setReportedUser(response.data.reportResult);
+      setTotalReported(response.data.totalCount);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     getUserData();
-  }, [page, isSearching, searchQuery]);
+  }, [userPage, isSearching, searchQuery]);
 
+  useEffect(() => {
+    if (activeindex === 2) {
+      getReportedReviewsData();
+    }
+  }, [activeindex]);
   return (
     <>
       <div className="w-full flex items-center justify-center h-[calc(100vh-65px)] ">
@@ -147,9 +181,9 @@ const AdminPage = () => {
                   />
                   <Pagination
                     total={totalUsers}
-                    value={page}
-                    onPageChange={handleChangePage}
-                    blockSize={totalPages}
+                    value={userPage}
+                    onPageChange={handleChangeUserPage}
+                    blockSize={blockSize}
                     pageSize={limit}
                     className="flex justify-center p-3 "
                   >
@@ -162,7 +196,51 @@ const AdminPage = () => {
                   </div>
                 </div>
               </Tabs.Pannel>
-              <Tabs.Pannel index={2}>신고리뷰</Tabs.Pannel>
+              <Tabs.Pannel index={2}>
+                <div className="p-10 w-full">
+                  <h1 className="text-2xl flex justify-center">
+                    신고 리뷰 관리
+                  </h1>
+                </div>
+                <div className="p-10 w-full">
+                  <div className="p-2 flex flex-row-reverse">
+                    <SearchBar
+                      onSearch={handleSearch}
+                      placeholder="회원 이름 검색"
+                      useDebounce={true}
+                    />
+                  </div>
+
+                  <Table
+                    columns={[
+                      { key: "reason", label: "신고 사유" },
+                      { key: "imgUrls", label: "리뷰 사진" },
+                      { key: "content", label: "리뷰 글" },
+                      { key: "email", label: "신고한 회원아이디" },
+                      {
+                        key: "reviewUserId",
+                        label: "신고당한 회원아이디",
+                      },
+                    ]}
+                    data={reportedUser}
+                    onSelectAll={handleSelectAllUsers}
+                    onSelectItem={handleSelectUser}
+                    selectedItems={selectedUsers}
+                  />
+                  <Pagination
+                    total={totalReported}
+                    value={reviewPage}
+                    onPageChange={handleChangeReviewPage}
+                    blockSize={blockSize}
+                    pageSize={limit}
+                    className="flex justify-center p-3 "
+                  >
+                    <Pagination.Navigator className="flex gap-4">
+                      <Pagination.Buttons className="PaginationButtons flex gap-4 font-bold text-slate-300" />
+                    </Pagination.Navigator>
+                  </Pagination>
+                </div>
+              </Tabs.Pannel>
             </div>
           </div>
         </Tabs>
